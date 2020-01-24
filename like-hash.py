0edd1c2ea1e5a3e430c7bf521a900deaf5534593
Massili Kezzoul <massy.kezzoul@gmail.com>
2020-01-24 01:25:03 +0100

    adding delay between likes

--- like-hash.py
+++ like-hash.py
@@ -1,8 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import NoSuchElementException
-from selenium.common.exceptions import ElementClickInterceptedException
-from selenium.common.exceptions import StaleElementReferenceException
 
 import time
 
@@ -11,13 +9,14 @@ class TwitterBot:
         self.username = username
         self.password = password
         self.bot = webdriver.Firefox()
+        self.likeDelay = 5
     
     def login(self):
         bot = self.bot
         bot.get('https://twitter.com/')
         bot.maximize_window()
         print("Connection en cours...")
-        time.sleep(2) # TODO in future try this : https://selenium-python.readthedocs.io/waits.html
+        time.sleep(4) # TODO in future find a more efficient wait method
         try:
             #email = bot.find_element_by_css_selector('input.email-input')
             email = bot.find_element_by_name('session[username_or_email]')
@@ -31,35 +30,42 @@ class TwitterBot:
         email.send_keys(self.username)
         pwd.send_keys(self.password)
         pwd.send_keys(Keys.RETURN)
-        time.sleep(1)
-
-    def like_tweet(self,hashtag):
+        time.sleep(4)
+        # TODO check connection done
+        print("Connection done.")
+    
+    def like_tweet(self,hashtag,toLike = 10):
         bot = self.bot
         bot.get('https://twitter.com/search?q=%23'+hashtag)
-        print("Chargement des tweets...")
-        time.sleep(1)
+        print("Entrain de liker des tweets...")
+        time.sleep(3)
         nb = 0
+        liked = 0
         impossible = 0
-        pas = 600 # TODO trouver la bonne taille pour scroller
+        pas = 300 # TODO trouver la bonne taille pour scroller
         scroll = 0
-        for i in range(1,20): # scroll for loading tweets
+        while liked <= toLike: # scroll for loading tweets
             #tweets = bot.find_elements_by_css_selector('article.css-1dbjc4n.r-1loqt21.r-1udh08x.r-o7ynqc.r-1j63xyz')
-            tweets = bot.find_elements_by_css_selector('article div[data-testid=like] svg')
+            tweets = bot.find_elements_by_css_selector('article div[data-testid=like] svg, div.HeartAnimation') # like button
+            nb += len(tweets)
             for tweet in tweets:
                 try:
                     tweet.click()
-                    time.sleep(5)
+                    liked += 1
+                    time.sleep(self.likeDelay)
                 except Exception:
                     impossible += 1
 
-            bot.execute_script('window.scrollTo('+str(scroll)+ ','+ str(scroll+pas) +')')
             scroll += pas
-            time.sleep(5)
-            nb += len(tweets)
+            bot.execute_script('window.scrollTo(0,'+ str(scroll) +')')
+            time.sleep(2) # waiting for element to load after a scroll
         print(str(nb) + " tweets au total.")
-        print(str(nb-impossible) + " tweet liker au total.")
+        print(str(liked) + " tweets liker au total.")
         print(str(impossible)+ " tweets non liker.")
-        bot.quit()
+        
+        
+    def quit(self):
+        self.bot.quit()
 
 
 mail = 'likehash@yahoo.com'
@@ -67,4 +73,8 @@ password = 'motdepasse00'
 
 bot = TwitterBot(mail,password)
 bot.login()
-bot.like_tweet('hashtag')
+hashtags = ['selenium','github','dev','botTwitter','bot']
+for hashtag in hashtags:
+    print("liking #"+hashtag+"...")
+    bot.like_tweet(hashtag,30)
+bot.quit()
